ad49746e95d1e26ab1395bdc614fbd038ffe08a4
jleonen <jleonen2012@gmail.com>
2021-10-28 01:53:27 -0700

    Added functionality to render project items onto app.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <div>
       <InputProject onAddProject={newProjectHandler} />
+      <AddProject projects={newProject} />
     </div>
   );
 }
--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ProjectItem from "./ProjectItem";
 
-const AddProject = function () {
-  return;
+const AddProject = function (props) {
+  return (
+    <ul>
+      {props.projects.map((project) => (
+        <li key={project.id}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default AddProject;
