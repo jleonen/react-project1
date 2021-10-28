4c3c75adca6842cfb9ea46843679a1a4d6c25f54
jleonen <jleonen2012@gmail.com>
2021-10-28 01:45:27 -0700

    Added function in App.js to process info from InputProject.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import AddProject from "./Components/AddProject";
 import InputProject from "./Components/InputProject";
 function App() {
-  const newProjectHandler = function (name, description) {
-    console.log(name, description);
+  const [newProject, setNewProject] = useState([]);
+  const newProjectHandler = function (projectName, desc) {
+    setNewProject((prevProjects) => {
+      return [
+        ...prevProjects,
+        { name: projectName, description: desc, id: Math.random().toString() },
+      ];
+    });
+    console.log(newProject);
   };
   return (
     <div>
