bf88d169d20faa5c12fe236be556c0f622dd250b
jleonen <jleonen2012@gmail.com>
2021-11-11 21:22:41 -0800

    Added function to have action item crossed out once completed.

--- src/Components/Action Items/ActionItem.js
+++ src/Components/Action Items/ActionItem.js
@@ -3,22 +3,27 @@ import { BiTrash } from "react-icons/bi";
 import style from "../Add Project/ProjectItem.module.css";
 const ActionItem = (props) => {
   const deleteHandler = (event) => props.onDeleteItem(props.id, event);
+  const completedProject = (event) => {
+    event.target.classList.toggle(style.completed);
+  };
   return (
     <div>
       <h3>{props.project}</h3>
       <li>
         {props.tasks.map((item, index) => (
           <div>
-            <p>{item}</p>
-            <button id={index} onClick={deleteHandler}>
+            <p className={style.taskItems} onClick={completedProject}>
+              {item}
+            </p>
+            {/* <button id={index} onClick={deleteHandler}>
               <BiTrash id={index} className={style.icon} />
-            </button>
+            </button> */}
           </div>
         ))}
-        {/* 
+
         <button onClick={deleteHandler}>
-          <BiTrash className={style.icon} />
-        </button> */}
+          <BiTrash className={style.icon} /> Delete Task List
+        </button>
       </li>
     </div>
   );
